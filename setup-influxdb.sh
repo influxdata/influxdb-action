290dc998ec9c2ddf7ed8ecf3b3fe1f3277b8f806
Russ Savage <russ@influxdata.com>
2021-06-04 16:51:30 -0700

    feat: fixing env names

--- setup-influxdb.sh
+++ setup-influxdb.sh
@@ -1,7 +1,7 @@
 influxd --http-bind-address :8086 --reporting-disabled > /dev/null 2>&1 &
 until curl -s http://localhost:8086/health; do sleep 1; done
 influx setup --host http://localhost:8086 -f \
-  -o ${{ inputs.influxdb_org }} \
-  -u ${{ inputs.influxdb_user }} \
-  -p ${{ inputs.influxdb_password }} \
-  -b ${{ inputs.influxdb_bucket }}
+  -o ${{ inputs.influxdb-org }} \
+  -u ${{ inputs.influxdb-user }} \
+  -p ${{ inputs.influxdb-password }} \
+  -b ${{ inputs.influxdb-bucket }}
