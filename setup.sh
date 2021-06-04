774733823538f849fb9a6caae0aef79f687c3724
Russ Savage <russ@influxdata.com>
2021-06-04 16:27:59 -0700

    feat: updating action to install influxdb

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
-echo "Goodbye"
+cd /tmp
+wget https://dl.influxdata.com/platform/nightlies/influx_nightly_linux_amd64.tar.gz
+tar xvfz influx_nightly_linux_amd64.tar.gz
+sudo cp influx_nightly_linux_amd64/{influx,influxd} /usr/local/bin/
+influxd --http-bind-address :8086 --reporting-disabled > /dev/null 2>&1 &
+until curl -s http://localhost:8086/health; do sleep 1; done
+influx setup --host http://localhost:8086 -f -b dummy -o influxdata -u ci_user -p password
+cd ~
