fca43e4d06d5be61963dece01e30e4bf7d23b9dd
Russ Savage <russ@influxdata.com>
2021-06-07 09:48:28 -0700

    fix: test pushing env vars

--- setup-influxdb.sh
+++ setup-influxdb.sh
@@ -2,7 +2,7 @@ influxd --http-bind-address :8086 --reporting-disabled > /dev/null 2>&1 &
 until curl -s http://localhost:8086/health; do sleep 1; done
 env
 influx setup --host http://localhost:8086 -f \
-  -o $INPUT_INFLUXDB_ORG \
-  -u $INPUT_INFLUXDB_USER \
-  -p $INPUT_INFLUXDB_PASSWORD \
-  -b $INPUT_INFLUXDB_BUCKET
+  -o $INFLUXDB_ORG \
+  -u $INFLUXDB_USER \
+  -p $INFLUXDB_PASSWORD \
+  -b $INFLUXDB_BUCKET
