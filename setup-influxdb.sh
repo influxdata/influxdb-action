45b6c3b55559af1cc8e97a707bcbe98301b1312f
Roms1383 <romain.kelifa@gmail.com>
2022-02-26 09:50:54 +0700

    feat: support api token

--- setup-influxdb.sh
+++ setup-influxdb.sh
@@ -3,9 +3,17 @@ if [ "$INFLUXDB_START" = "true" ]
 then
     influxd --http-bind-address :8086 --reporting-disabled > /dev/null 2>&1 &
     until curl -s http://localhost:8086/health; do sleep 1; done
-    influx setup --host http://localhost:8086 -f \
-        -o $INFLUXDB_ORG \
-        -u $INFLUXDB_USER \
-        -p $INFLUXDB_PASSWORD \
-        -b $INFLUXDB_BUCKET
+    if [ -z ${INFLUXDB_TOKEN+x} ];
+        then influx setup --host http://localhost:8086 -f \
+            -o $INFLUXDB_ORG \
+            -u $INFLUXDB_USER \
+            -p $INFLUXDB_PASSWORD \
+            -b $INFLUXDB_BUCKET
+        else influx setup --host http://localhost:8086 -f \
+            -o $INFLUXDB_ORG \
+            -u $INFLUXDB_USER \
+            -p $INFLUXDB_PASSWORD \
+            -b $INFLUXDB_BUCKET \
+            -t $INFLUXDB_TOKEN
+    fi
 fi
